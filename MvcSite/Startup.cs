ec2edf0e5e343af870394a1d87b1d2ec70907301
Will 保哥 <doggy.huang@gmail.com>
2016-09-22 19:12:10 +0800

    加入「MVC」專案

---
+++ MvcSite/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(MvcSite.Startup))]
+namespace MvcSite
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
