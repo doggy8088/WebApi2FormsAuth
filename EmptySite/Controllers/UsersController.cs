54883d6deeaea33dc8424819eca3018fc2d3f1c7
Will 保哥 <doggy.huang@gmail.com>
2016-09-22 19:08:32 +0800

    加入 表單驗證 ( FormsAuthenticaion ) 功能

---
+++ EmptySite/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using EmptySite.Models;
+using System.Net;
+using System.Web.Http;
+using System.Web.Security;
+
+namespace EmptySite.Controllers
+{
+    [RoutePrefix("user")]
+    public class UsersController : ApiController
+    {
+        [Route("login")]
+        public IHttpActionResult PostLogin(LoginViewModel user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            if (user.Username == "admin" && user.Password == "123")
+            {
+                FormsAuthentication.RedirectFromLoginPage(user.Username, false);
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+            else
+            {
+                return StatusCode(HttpStatusCode.Unauthorized);
+            }
+        }
+
+        [Route("logout")]
+        [HttpGet][HttpPost]
+        public void Logout()
+        {
+            FormsAuthentication.SignOut();
+        }
+
+        [Authorize]
+        [Route("profile")]
+        public IHttpActionResult GetProfile()
+        {
+            return Ok(User.Identity.Name);
+        }
+    }
+}
---
+++ EmptySite/Models/LoginViewModel.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace EmptySite.Models
+{
+    public class LoginViewModel
+    {
+        [Required]
+        [StringLength(20)]
+        public string Username { get; set; }
+        [Required]
+        public string Password { get; set; }
+    }
+}
