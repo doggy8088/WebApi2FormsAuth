3190e640dcbfd01e83333151169f271446e00498
Will 保哥 <doggy.huang@gmail.com>
2016-09-22 19:29:16 +0800

    加入 表單驗證 ( FormsAuthenticaion ) 功能

---
+++ WebApiSite/Controllers/UsersController.cs
@@ -0,0 +1,44 @@
+﻿using WebApiSite.Models;
+using System.Net;
+using System.Web.Http;
+using System.Web.Security;
+
+namespace WebApiSite.Controllers
+{
+    [RoutePrefix("user")]
+    public class UsersController : ApiController
+    {
+        [Route("login")]
+        public IHttpActionResult PostLogin(UserLoginViewModel user)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+
+            if (user.Username == "admin" && user.Password == "123")
+            {
+                FormsAuthentication.RedirectFromLoginPage(user.Username, false);
+                return StatusCode(HttpStatusCode.NoContent);
+            }
+            else
+            {
+                return StatusCode(HttpStatusCode.Unauthorized);
+            }
+        }
+
+        [Route("logout")]
+        [HttpGet][HttpPost]
+        public void Logout()
+        {
+            FormsAuthentication.SignOut();
+        }
+
+        [Authorize]
+        [Route("profile")]
+        public IHttpActionResult GetProfile()
+        {
+            return Ok(User.Identity.Name);
+        }
+    }
+}
